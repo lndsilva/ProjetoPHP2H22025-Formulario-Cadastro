b0b9da366d5c549e22235e409a442122e7a57059
lndsilva <lndsilva@hotmail.com>
2025-10-21 21:33:47 -0300

    formulário de cadastro em PHP com bootstrap

--- include/cadastrar.php
+++ include/cadastrar.php
@@ -1,6 +1,14 @@
 <?php 
 
-	
+	$primeiroNome = $_POST['$primeiroNome'];
+	$segundoNome = $_POST['$segundoNome'];
+	$escolhaPets = $_POST['$escolhaPets'];
+
+
+	echo($primeiroNome . " - " 
+		. $segundoNome ." - " 
+		. $escolhaPets);
+
 
 
  ?>
